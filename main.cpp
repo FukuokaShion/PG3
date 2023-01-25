dad72e56533c917e34a3fad7ac62cec1f87b58a9
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-01-25 12:24:04 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -1,75 +1,38 @@
-#include "stdio.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include "windows.h"
+#include <time.h>
+#include <Windows.h>
+#include <functional>
 
-typedef void (*PFunc)();
-
-//コールバック関数
-void Correct() {
-	printf("正解\n");
-
-}
-
-//コールバック関数
-void Incorrect() {
-	printf("はずれ\n");
-
-}
-
-
-void setTimeout(PFunc p, int second) {
-	//待機
+void SetTimeout(std::function<void()> func, int second){
+	printf("%d秒待機\n", second);
 	Sleep(second * 1000);
 
-	//呼び出し
-	p();
+	func();
 
 }
 
+int main(int argc, const char* argv[]){
+	int select;
+	printf("偶数だと思うなら0、奇数なら1を入力\n");
+	scanf_s("%d", &select);
 
-int main()
-{
-	//入力
-	int anser = 0;
+	int dice = rand() % 6 + 1;
 
-	//関数ポインタ
-	PFunc p;
-	p = Correct;
-
-	//さいころ
-	int	dice = rand() % 6 + 1;
-	int remainder = dice % 2;
-
-
-	printf("偶数なら1、奇数なら-1を入力\n");
-	scanf_s("%d", &anser);
-
-	//選択が偶数なら
-	if (anser == 1) {
-		printf("偶数を選択\n");
-		//賽の目が偶数なら
-		if (remainder == 0) {
-			p = Correct;
+	//関数の作成
+	std::function<void()> judgeFunc = [&](){
+		if (dice % 2 == select) {
+			printf("%dで正解\n", dice);
+		}else {
+			printf("%dで不正解\n", dice);
 		}
-		else {
-			p = Incorrect;
-		}
-	}
-	else if (anser == -1) {
-		printf("奇数を選択\n");
-		//賽の目が奇数なら
-		if (remainder == 1) {
-			p = Correct;
-		}
-		else {
-			p = Incorrect;
-		}
-	}
+	};
 
-	//コールバック
-	setTimeout(p, 3);
-	printf("賽の目は%d\n", dice);
 
+	SetTimeout(judgeFunc, 3);
+
+
+	system("pause");
 
 	return 0;
 }
